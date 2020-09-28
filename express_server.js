2d02b93bfbb4486b4b36858b3ece3b656ce4a939
vagrant <shadmerhi@gmail.com>
2020-09-28 17:00:17 +0000

    Added /urls and its ejs view file

--- express_server.js
+++ express_server.js
@@ -18,10 +18,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
