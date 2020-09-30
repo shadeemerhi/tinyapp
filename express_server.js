9ae57db1f633145f323032423966081748a2e9a8
vagrant <shadmerhi@gmail.com>
2020-09-30 01:49:35 +0000

    Added registration form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan');
 const PORT = 8080;
 
 const bodyParser = require("body-parser");
@@ -12,6 +11,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {};
 
 app.set('view engine', 'ejs');
 
@@ -26,7 +27,7 @@ app.listen(PORT, () => {
 app.get('/urls', (req, res) => {
 
   let templateVars = {
-    username: req.cookies['username'],
+    user: users[req.cookies['user_id']],
     urls: urlDatabase
   };
   res.render('urls_index', templateVars);
@@ -34,14 +35,14 @@ app.get('/urls', (req, res) => {
 
 app.get('/urls/new', (req, res) => {
   const templateVars = {
-    username: req.cookies['username']
+    user: users[req.cookies['user_id']]
   }
   res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
-    username: req.cookies['username'],
+    user: users[req.cookies['user_id']],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -79,9 +80,29 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
-})
+});
+
+app.get('/register', (req, res) => {
+  let templateVars = {
+    user: users[req.cookies['user_id']],
+    urls: urlDatabase
+  };
+  res.render('register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  const userID = generateRandomString();
+  const newUser = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password
+  }
+  users[userID] = newUser;
+  res.cookie('user_id', userID);
+  res.redirect('/urls');
+});
 
 function urlChecker (url) {
   if (!url.includes('http://') && !url.includes('www')) {
